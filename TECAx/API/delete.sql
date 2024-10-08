358589ed5a22f718691780a7503314936900239c
JoselynVMR <vmrjoselyn@gmail.com>
2024-10-08 01:49:08 -0600

    avance bd e info

--- TECAx/API/delete.sql
+++ TECAx/API/delete.sql
@@ -4,13 +4,29 @@ VALUES ('Formulario de Inscripción', 'Formulario para inscribirse en el program
 
 -- Insert into Recursos_Adicionales
 INSERT INTO Recursos_Adicionales (nombre, descripcion, URL, imagen, idFormulario)
-Values('CONAPDIS', 'El Conapdis es el rector en discapacidad, responsable de promover y fiscalizar el cumplimiento de los derechos humanos de la población con discapacidad, para fomentar su desarrollo inclusivo en todos los ámbitos de la sociedad.', 'https://conapdis.go.cr', '../assets/CONAPDIS.png', 1),
-('Wheelmap', 'Wheelmap es una plataforma en línea que permite a los usuarios encontrar y compartir información sobre la accesibilidad de lugares en todo el mundo.', 'https://wheelmap.org', '../assets/WheelMap.png', 1),
-('Blog de Apps para Personas con Discapacidad', 'Blog que ofrece información sobre aplicaciones móviles y tecnología accesible para personas con discapacidad.', 'https://gaptain.com/blog/18-herramientas-digitales-para-personas-con-discapacidad/', '../assets/18.png', 1);
+Values('CONAPDIS', 'El Conapdis es el rector en discapacidad, responsable de promover y fiscalizar el cumplimiento de los derechos humanos de la población con discapacidad, para fomentar su desarrollo inclusivo en todos los ámbitos de la sociedad.', 'https://conapdis.go.cr', '../assets/CONAPDIS.png', 2),
+('Wheelmap', 'Wheelmap es una plataforma en línea que permite a los usuarios encontrar y compartir información sobre la accesibilidad de lugares en todo el mundo.', 'https://wheelmap.org', '../assets/WheelMap.png', 2),
+('Blog de Apps para Personas con Discapacidad', 'Blog que ofrece información sobre aplicaciones móviles y tecnología accesible para personas con discapacidad.', 'https://gaptain.com/blog/18-herramientas-digitales-para-personas-con-discapacidad/', '../assets/18.png', 2);
 
 -- Insert into Informacion
-INSERT INTO Informacion (tipo, titulo, descripcion, idRecurso)
-VALUES ('General', 'Introducción al Programa', 'Un documento introductorio sobre el programa.', 1);
+-- INSERT INTO Informacion (tipo, titulo, descripcion)
+-- VALUES ('General', 'Introducción al Programa', 'Un documento introductorio sobre el programa.');
+INSERT INTO Informacion (tipo,titulo,descripcion) VALUES (1,'¿Qué es','La Comisión de Equiparación de Oportunidades es el órgano oficial del Instituto Tecnológico de Costa Rica que se encarga de promover y garantizar la inclusión y la igualdad de oportunidades para las personas con discapacidad en la institución.');
+
+INSERT INTO Informacion (tipo,titulo,descripcion, nombreRecurso, linkRecurso, descripcionRecurso) VALUES (2,'Funcionalidades','Velar por el cumplimiento de lo estipulado en la Ley de Igualdad de Oportunidades para las Personas con Discapacidad, Ley No. 7600 y su reglamento. Ingrese al siguiente enlace si desea saber más información sobre leyes.','información sobre leyes.','/leyes','Enlace que le redirige a la sección de normas');
+INSERT INTO Informacion (tipo,titulo,descripcion) VALUES (2,'Funcionalidades','Promover en la Comunidad Institucional el desarrollo de mayores niveles de sensibilización y concientización sobre los Derechos Humanos y la Discapacidad.');
+
+INSERT INTO Informacion (tipo,titulo,descripcion, nombreRecurso, linkRecurso, descripcionRecurso) VALUES (3,'Servicios','Acompañamiento y asesoría psicoeducativa individual y grupal para estudiantes con necesidades educativas y en condición de discapacidad.','','../assets/servicios1.png','Se muestra dos personas hablando. Una persona está sentada frente a un escritorio y la otra está en una silla de ruedas.');
+INSERT INTO Informacion (tipo,titulo,descripcion, nombreRecurso, linkRecurso, descripcionRecurso) VALUES (3,'Servicios','Gestión de ajustes y apoyos educativos para el aprendizaje según las necesidades de cada estudiante.','','../assets/servicios2.png','Se muestra una persona en silla de ruedas frente a un escritorio.');
+INSERT INTO Informacion (tipo,titulo,descripcion, nombreRecurso, linkRecurso, descripcionRecurso) VALUES (3,'Servicios','Gestión de acciones y articulación con otras instancias y programas que permitan la revisión, definición y actualización de acciones para el cumplimiento de los derechos de la población estudiantil con necesidades educativas y en condición de discapacidad.','','../assets/servicios3.png','Se muestran cuatro manos levantadas.');
+INSERT INTO Informacion (tipo,titulo,descripcion, nombreRecurso, linkRecurso, descripcionRecurso) VALUES (3,'Servicios','Asesoría en el tema de apoyos educativos, discapacidad y necesidades educativas a docentes, estudiantes u otras instancias que lo requieran.','','../assets/servicios4.png','Se muestra una clase, hay un profesor señalando algo en un pizarra frente a un grupo de personas.');
+
+INSERT INTO Informacion (tipo,titulo,descripcion) VALUES (4,'Integrantes','La siguiente tabla contiene los datos de los integrantes de la comisión:');
+
+INSERT INTO Informacion (tipo,titulo,descripcion, linkRecurso, descripcionRecurso) VALUES (5,'27 de Noviembre 2023',
+'“Siempre van a existir las posibilidades y no hay límites"', '');
+
+
 
 -- Insert into Preguntas
 INSERT INTO Preguntas (pregunta, idFormulario, tipoRespuesta, opcionesRespuesta) VALUES
@@ -34,8 +50,12 @@ INSERT INTO Preguntas (pregunta, idFormulario, tipoRespuesta, opcionesRespuesta)
 ('Acompañamiento',2,3,'Clínica de Atención Integral en Salud (CAIS), Comisión Institucional de Salud Ocupacional (CISO), Caja Costarricense de Seguro Social, Instituto Nacional de Seguros, Otro');
 
 -- Insert into Miembros
-INSERT INTO Miembros (nombre, email, telefono, sede)
-VALUES ('Juan Pérez', 'juan.perez@example.com', '555-1234', 'Sede Central');
+INSERT INTO Miembros (nombre, sede, email, telefono)
+VALUES ('Paula Cubillo Segura', 'Cartago', 'pcubillo@tec.ac.cr', '2550-2784');
+
+INSERT INTO Miembros (nombre, sede, email, telefono)
+VALUES ('Ericka Solano', 'San José', 'es@tec.ac.cr', '2555-4781');
+
 
 -- Insert into FormulariosRecibidos
 INSERT INTO FormulariosRecibidos (tipoUsuario, idUsuario, resouestas, email, sede, fecha, idFormulario, idEncargado)
--- TECAx/src/app/info/info.component.ts
+++ TECAx/src/app/info/info.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+interface Informacion {
+  id: number;
+  tipo: number;
+  titulo: string;
+  descripcion: string;
+  nombrerecurso: string;
+  linkrecurso: string;
+  descripcionrecurso: string;
+};
+
+interface Miembros {
+  id: number;
+  nombre: string;
+  sede: string;
+  email: string;
+  telefono: string;
+};
+
 @Component({
   selector: 'app-info',
   standalone: true,
@@ -8,6 +26,76 @@ import { RouterModule } from '@angular/router';
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
+
 export class InfoComponent {
 
+  info: Informacion[] = [];
+  infoGeneral: Informacion[] = [];
+  listaPunto: Informacion[] = [];
+  listaImg: Informacion[] = [];
+  infoTabla: Informacion[] = [];
+  tabla: Miembros[] = [];
+
+  noticias: Informacion[] = [];
+  
+
+  listasPuntos: { [titulo: string]: Informacion[] } = {};
+  listasImg: { [titulo: string]: Informacion[] } = {};
+
+  constructor() {}
+
+  async ngOnInit() {
+    await fetch('http://localhost:8080/info/all', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.info = data;
+        console.log(this.info.map((info: Informacion) => info.descripcion));
+
+        this.infoGeneral = this.info.filter(q => q.tipo === 1);
+        this.listaPunto = this.info.filter(q => q.tipo === 2);
+        this.listaImg = this.info.filter(q => q.tipo === 3);
+        this.infoTabla = this.info.filter(q => q.tipo === 4);
+        this.noticias = this.info.filter(q => q.tipo === 5);
+
+        this.listaPunto.forEach(item => {
+          if (!this.listasPuntos[item.titulo]) {
+            this.listasPuntos[item.titulo] = [];
+          }
+          this.listasPuntos[item.titulo].push(item);
+        });
+
+        this.listaImg.forEach(item => {
+          if (!this.listasImg[item.titulo]) {
+            this.listasImg[item.titulo] = [];
+          }
+          this.listasImg[item.titulo].push(item);
+        });
+
+      });
+
+      await fetch('http://localhost:8080/miem/all', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          this.tabla = data;
+          console.log(this.tabla.map((tabla: Miembros) => tabla.sede));  
+        });
+  }
+
+  getTitulosListasPuntos(): string[] {
+    return Object.keys(this.listasPuntos);
+  }
+
+  getTitulosListasImg(): string[] {
+    return Object.keys(this.listasImg);
+  }
 }
